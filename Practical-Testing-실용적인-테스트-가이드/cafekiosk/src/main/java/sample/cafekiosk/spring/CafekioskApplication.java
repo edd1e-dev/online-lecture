d13d39e656842c753356e662f2cd664aef415997
ksw <ksw@codeblock.co.kr>
2023-10-29 22:33:43 +0900

    update

--- Practical-Testing-실용적인-테스트-가이드/cafekiosk/src/main/java/sample/cafekiosk/spring/CafekioskApplication.java
+++ Practical-Testing-실용적인-테스트-가이드/cafekiosk/src/main/java/sample/cafekiosk/spring/CafekioskApplication.java
@@ -2,12 +2,14 @@ package sample.cafekiosk.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CafekioskApplication {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(CafekioskApplication.class, args);
+		SpringApplication.run(CafekioskApplication.class, args);
 	}
 
 }
